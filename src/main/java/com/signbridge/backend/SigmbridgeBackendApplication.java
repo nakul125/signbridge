14b89ea2ae1edcfa448e3c13a14b12ecc38f9e8f
Nakul <nakulwakode191@gmail.com>
2026-06-06 21:26:46 +0530

    feat:initial Spring Boot project setup Added Spring Web, JPA, Security, WebSocket dependencies Configured package structure Added .gitignore

---
+++ src/main/java/com/signbridge/backend/SigmbridgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.signbridge.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmbridgeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigmbridgeBackendApplication.class, args);
+    }
+
+}
