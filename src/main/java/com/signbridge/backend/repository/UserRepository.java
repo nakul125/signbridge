42d044a27df7e8d9bc443aeb9e9bd71a51b1c52e
Nakul <nakulwakode191@gmail.com>
2026-06-07 19:28:38 +0530

    remove application.properties from git tracking

---
+++ src/main/java/com/signbridge/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.signbridge.backend.repository;
+
+import com.signbridge.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface UserRepository {
+
+}
