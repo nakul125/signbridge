906af24aaa94064eef51fce51c7e1e88d0016f33
Nakul <nakulwakode191@gmail.com>
2026-06-11 00:09:52 +0530

    chore: created exceptions classes

---
+++ src/main/java/com/signbridge/backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.signbridge.backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String resource,
+                                     String field,
+                                     Object value) {
+        super(String.format("%s not found with %s: '%s'", resource, field, value));
+    }
+}
