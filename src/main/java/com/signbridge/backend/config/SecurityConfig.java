3af6e99d52e8c78e02e0abd8855f56951e657367
Nakul <nakulwakode191@gmail.com>
2026-06-14 23:53:46 +0530

    chore: added Appconfig class to encrypt the Password

---
+++ src/main/java/com/signbridge/backend/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.signbridge.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
