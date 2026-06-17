ee7c258e44291563aa03e36da562b19cdb1a9f7a
Nakul <nakulwakode191@gmail.com>
2026-06-17 18:35:43 +0530

    feat: add password encoder and temporary open security config

--- src/main/java/com/signbridge/backend/config/SecurityConfig.java
+++ src/main/java/com/signbridge/backend/config/SecurityConfig.java
@@ -11,4 +11,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+
 }
